bf0964d7a091b14af71f3432e94296b22f685b47
1825priya <79133241+1825priya@users.noreply.github.com>
2026-07-30 14:33:20 +0530

    Add files via upload

---
+++ starter/solution.sql
@@ -0,0 +1,9 @@
+-- ********************************************
+-- Assignment:
+-- Create CollegeDB and Department Table
+-- ********************************************
+
+-- SQLite does not support CREATE DATABASE.
+-- The database file CollegeDB.db will be created automatically.
+
+-- Write your SQL statements below.
